f8fc49f25fa0fed0d502c059f288ecfff35d4c93
Danielle <danielle.pichlis@gmail.com>
2013-02-28 01:28:01 +0200

    Showing lakes in the map. Missing geolocation

--- djangoproject/static/js/views/SearchByMapView.js
+++ djangoproject/static/js/views/SearchByMapView.js
@@ -3,13 +3,29 @@
 window.WiserWater.SearchByMapView = Backbone.View.extend({
   initialize: function(options) {
     this.template = _.template(WiserWater.tpl.get('searchmap'));
+    console.debug("Loading lakes");
+    this.allLakes = new window.WiserWater.LakeCollection();
     return this;
   },
   events: {
     "click .searchName": "onSearchNameClick"
   },
   render: function() {
-    $(this.el).html(this.template);
+    var self;
+    self = this;
+    $(this.el).html(this.template());
+    this.allLakes.fetch({
+      success: function(fetchedLakes) {
+        return _.each(self.allLakes.models, (function(item) {
+          var lakePosition;
+          lakePosition = new google.maps.LatLng(item.getLocation().lat, item.getLocation().lon);
+          return $("#map_canvas").gmap("addMarker", {
+            position: lakePosition,
+            bounds: true
+          });
+        }), this);
+      }
+    });
     return this;
   },
   onSearchNameClick: function(args) {
